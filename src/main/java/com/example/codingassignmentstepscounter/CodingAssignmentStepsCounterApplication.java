709f9fe0fe9f1217ed7c063da0aebbd9300b109f
aleh.vilchynski <vilchinskioleg@gmail.com>
2025-11-07 20:19:18 +0100

    CA-StCount: init project

---
+++ src/main/java/com/example/codingassignmentstepscounter/CodingAssignmentStepsCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingassignmentstepscounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAssignmentStepsCounterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodingAssignmentStepsCounterApplication.class, args);
+  }
+
+}
